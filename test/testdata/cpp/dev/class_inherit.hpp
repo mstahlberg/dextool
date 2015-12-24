ae7c5af8b45763702640a767e55fab832f126280
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2015-12-24 02:01:17 +0100

    WIP updated test case for inherit visit with expected result

--- test/testdata/cpp/dev/class_inherit.hpp
+++ test/testdata/cpp/dev/class_inherit.hpp
@@ -1,41 +1,67 @@
 // test data to understand clang AST regarding inheritance
 
+// expect gmock
 class A {
-    void a() {}
+public:
+    virtual ~A() {}
+    virtual void a() = 0;
 };
 
+// expect NO gmock
+// even though A is an interface skipping B.
+// this is considered the type of dangerous territory
+// that we do not want to generate a gmock for.
+// if the SUT take a ref to B then it is "stupid", not
+// an interface. It should have been a ref to A.
 class B : public A {
+public:
     void b() {}
 };
 
+// expect NO gmock.
+// See explanation for B.
 class C : public B {
+public:
     void c() {}
 };
 
+// expect gmock
 class VirtA {
+public:
     virtual ~VirtA() {}
-
-    virtual void virtA() {}
+    virtual void virtA() = 0;
 };
 
+// expect gmock with all from VirtA
 class VirtB : public VirtA {
-    virtual void virtB() {}
+public:
+    virtual ~VirtB() {}
+    virtual void virtB() = 0;
 };
 
+// expect gmock with all from VirtA and VirtB
 class VirtC : public VirtB {
-    virtual void virtA() {}
-    virtual void virtB() {}
+public:
+    virtual ~VirtC() {}
+    virtual void virtC() = 0;
 };
 
 namespace ns1 {
 
+// expect gmock
 class Ns1A {
-    void a() {}
+public:
+    virtual ~Ns1A() {}
+    virtual void a() = 0;
 };
 
 namespace ns2 {
 
+// expect gmock, all from Ns1A.
+// correct inheritance reference to Ns1A
 class Ns2B : public Ns1A {
+public:
+    virtual ~Ns2B() {}
 };
 
 } // NS: ns2
